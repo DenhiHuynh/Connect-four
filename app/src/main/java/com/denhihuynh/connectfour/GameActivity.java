4d3af2c02a259c60e873e94020ae592ef9d24168
TabTabTab <hansjohannyholm@gmail.com>
2015-10-01 18:41:34 +0200

    Added disc dropping functionality

--- app/src/main/java/com/denhihuynh/connectfour/GameActivity.java
+++ app/src/main/java/com/denhihuynh/connectfour/GameActivity.java
@@ -1,30 +1,30 @@
 package com.denhihuynh.connectfour;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.ActivityInfo;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
-import com.denhihuynh.connectfour.R;
-
-import interfaces.OnFragmentInteractionListener;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+    final static long INTERVAL = 500;
+    final static long TIMEOUT = 5000;
     private TableLayout tableLayout;
+    private ArrayList<TableRow> gameBoardTable;
+    private DiscDropper discDropper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gameBoardTable = new ArrayList<>();
         tableLayout = (TableLayout) findViewById(R.id.gameTable);
         addGameBoardRows();
     }
@@ -35,8 +35,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void addGameBoardRows() {
         LayoutInflater inflater = getLayoutInflater();
-        for(int i = 0;i<6;i++){
-            TableRow rowView =(TableRow) inflater.inflate(R.layout.tablerow_game, null, false);
+        for (int i = 0; i < 6; i++) {
+            TableRow rowView = (TableRow) inflater.inflate(R.layout.tablerow_game, null, false);
             Button col1 = (Button) rowView.getChildAt(0);
             Button col2 = (Button) rowView.getChildAt(1);
             Button col3 = (Button) rowView.getChildAt(2);
@@ -50,33 +50,129 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             col5.setOnClickListener(this);
             col6.setOnClickListener(this);
             tableLayout.addView(rowView);
+            gameBoardTable.add(rowView);
         }
     }
-
-
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        int col = 0;
+        switch (v.getId()) {
             case R.id.button_column_1:
+                col = 0;
                 System.out.println("---------------------------- col1");
                 break;
             case R.id.button_column_2:
+                col = 1;
                 System.out.println("---------------------------- col2");
                 break;
             case R.id.button_column_3:
+                col = 2;
                 System.out.println("---------------------------- col3");
                 break;
             case R.id.button_column_4:
+                col = 3;
                 System.out.println("---------------------------- col4");
                 break;
             case R.id.button_column_5:
+                col = 4;
                 System.out.println("---------------------------- col5");
                 break;
             case R.id.button_column_6:
+                col = 5;
                 System.out.println("---------------------------- col6");
                 break;
         }
+        dropDisc(col);
     }
+
+    /**
+     * Method to perform dropping disc animation.
+     *
+     * @param col the column to drop a disc into
+     */
+    private void dropDisc(final int col) {
+        //TODO fixa korrekt shite
+        if (discDropper == null || discDropper.isFinished()) {
+            discDropper = new DiscDropper(5, col, R.drawable.rounded_corner_red);
+            discDropper.start();
+        } else {
+            discDropper.stop();
+            discDropper = null;
+        }
+    }
+
+
+/**
+ * Class for handling dropping of discs into game board.
+ */
+private class DiscDropper {
+    private TimerTask timerTask;
+    private int rowIndex;
+    private int col;
+    private int drawableId;
+    private int destinationRowIndex;
+    private boolean isFinished;
+
+    public DiscDropper(final int destinationRowIndex, final int col, final int drawableId) {
+        this.col = col;
+        this.drawableId = drawableId;
+        this.destinationRowIndex = destinationRowIndex;
+        rowIndex = 0;
+        isFinished = false;
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (rowIndex < destinationRowIndex) {
+                    GameActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            gameBoardTable.get(rowIndex).getChildAt(col).setBackgroundResource(R.drawable.rounded_corner_white);
+                            gameBoardTable.get(rowIndex + 1).getChildAt(col).setBackgroundResource(drawableId);
+                            rowIndex++;
+                        }
+                    });
+                } else {
+                    isFinished = true;
+                    cancel();
+                }
+            }
+        };
+    }
+
+    /**
+     * Starts the discdropper. If the disc dropper is already started, then the disc is instantly dropped to the bottom.
+     */
+    public void start() {
+        gameBoardTable.get(0).getChildAt(col).setBackgroundResource(drawableId);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, INTERVAL, INTERVAL);
+    }
+
+    /**
+     * Instantly drops the disc to the correct position
+     */
+    public void stop() {
+        timerTask.cancel();
+
+        if (rowIndex < 6)
+            gameBoardTable.get(rowIndex).getChildAt(col).setBackgroundResource(R.drawable.rounded_corner_white);
+
+        if (rowIndex < 5)
+            gameBoardTable.get(rowIndex + 1).getChildAt(col).setBackgroundResource(R.drawable.rounded_corner_white);
+
+        gameBoardTable.get(destinationRowIndex).getChildAt(col).setBackgroundResource(drawableId);
+    }
+
+    /**
+     * Checks if the timertask has finished.
+     *
+     * @return if the timertask has finished.
+     */
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+}
 }
